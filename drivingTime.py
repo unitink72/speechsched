4096b8d10b2d69da365f9a5383aef71bc23d3fa8
Josh Tinkham <unitink72@gmail.com>
2018-01-08 16:22:58 +0000

    Print error upon non-numeric api result.  Don't bomb

--- drivingTime.py
+++ drivingTime.py
@@ -50,7 +50,8 @@ class DrivingTime:
       #print ('Got distance %d minutes' % distMins)
       return distMins
     else:
-      return None
+      print ('Error in distance lookup between\n%s\n%s' % (origStr,destStr)
+      return 1
     #end if
 
   #end getDist
